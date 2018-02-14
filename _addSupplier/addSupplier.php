96ff9f5ef746084c77f7c95dc3dbf31815876933
Rhyder Quinlan <rhyderquinlan@gmail.com>
2018-02-14 15:10:14 +0000

    commit 14/02

---
+++ _addSupplier/addSupplier.php
@@ -0,0 +1,12 @@
+<html>
+  <body>
+    <? php
+     echo "Supplier name: " . $_POST[supName];
+     echo "Address: " . $_POST[street] . ", " . $_POST[town] . ", " . $_POST[county];
+     echo "Email Adress: " . $_POST[email];
+     echo "Website: " . $_POST[website];
+     echo "Telephone: " . $_POST[teleNum];
+
+     ?>
+  </body>
+</html>
--- addSupplier.php
+++
@@ -1,6 +0,0 @@
-<?php
-  include 'db.inc.php';
-
-  $sql = "Insert into Drugs (Supplier Name, Supplier Location, Tel-Num)
-  VALUES ('$_POST[]','$_POST[gname]','$_POST[form]','$_POST[strength]','$_POST[sID]','$_POST[instruc]','$_POST[effects]',$_POST[cprice],$_POST[rprice],$_POST[relevel],$_POST[requantity],$_POST[sCODE])";
- ?>
